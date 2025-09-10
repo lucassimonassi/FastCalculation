3942b6fa20cace49f9f9b9a1e25247872a85070f
lucas.simonassi <lucas.simonassi@gmail.com>
2025-09-10 12:06:02 -0300

    Implementar a tela de configurações e a estrutura inicial do jogo
    - Criada a tela de configurações (SettingsActivity), com o layout e a lógica para o usuário definir as opções do jogo.
    - Adicionada a classe de dados Settings.kt para passar as informações entre as telas.
    - Adicionado o plugin 'kotlin-parcelize' para permitir que a classe Settings seja enviada via Intent.
    - Criado o objeto Extras.kt para as constantes.
    - Adicionada a estrutura inicial da tela de jogo (activity_game.xml) para receber o conteúdo futuro.
    - Atualizado o targetSdk para a versão 35 para garantir a compatibilidade e as otimizações mais recentes.

--- app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/SettingsActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package br.edu.scl.ifsp.sdm.fastcalculation
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.scl.ifsp.sdm.fastcalculation.Extras.EXTRA_SETTINGS
 import br.edu.scl.ifsp.sdm.fastcalculation.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,5 +16,25 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activitySettingsBinding.root)
+
+        setSupportActionBar(activitySettingsBinding.gameTbIn.gameTb)
+        supportActionBar?.title = getString(R.string.app_name)
+        supportActionBar?.subtitle = getString(R.string.settings)
+
+        activitySettingsBinding.apply {
+            letsGoBt.setOnClickListener {
+                val settings = Settings(
+                    playerNameEt.text.toString(),
+                    (roundsSp.selectedView as TextView).text.toString().toInt(),
+                    roundIntervalRg.run {
+                        findViewById<RadioButton>(checkedRadioButtonId).text.toString().toLong() * 1000L
+                    }
+                )
+                val gameActivityIntent = Intent(this@SettingsActivity, GameActivity::class.java)
+                gameActivityIntent.putExtra(EXTRA_SETTINGS, settings)
+                startActivity(gameActivityIntent)
+                finish()
+            }
+            }
+        }
     }
-}
