a4c84b401eeb7e649c8dda69270f52d2923f7f66
lucas.simonassi <lucas.simonassi@gmail.com>
2025-09-10 12:21:18 -0300

    Criar classe GameActivity
    - Adicionada a classe GameActivity para o funcionamento da tela de jogo.
    - Implementado o View Binding para gerenciar o layout.
    - Configurada a Action Bar da tela de jogo com título e subtítulo.

--- app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/GameActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/fastcalculation/GameActivity.kt
@@ -5,16 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.scl.ifsp.sdm.fastcalculation.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    private val activityGameBinding: ActivityGameBinding by lazy {
+        ActivityGameBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(activityGameBinding.root)
+
+        setSupportActionBar(activityGameBinding.gameTbIn.gameTb)
+        supportActionBar?.apply {
+            title = getString(R.string.app_name)
+            subtitle = getString(R.string.game)
         }
     }
 }
